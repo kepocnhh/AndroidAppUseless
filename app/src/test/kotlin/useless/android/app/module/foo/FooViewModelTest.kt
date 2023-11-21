7e0d9511514b22fe6f143612f5f426eb5abd609c
Stanley Wintergreen <kepocnhh@gmail.com>
2023-11-21 21:58:05 +0300

    request state test

--- app/src/test/kotlin/useless/android/app/module/foo/FooViewModelTest.kt
+++ app/src/test/kotlin/useless/android/app/module/foo/FooViewModelTest.kt
@@ -5,24 +5,40 @@ import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.take
 import kotlinx.coroutines.flow.withIndex
 import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertNull
 import org.junit.Test
+import useless.android.app.module.app.mockInjection
+import useless.android.app.provider.mockContexts
 import kotlin.time.Duration.Companion.seconds
 
 internal class FooViewModelTest {
     @Test
-    fun foo() {
+    fun requestStateTest() {
         runTest(timeout = 10.seconds) {
-            val injection = TODO()
+            val injection = mockInjection(
+                contexts = mockContexts(main = coroutineContext),
+            )
             val viewModel = FooViewModel(injection)
             viewModel
                 .state
                 .withIndex()
                 .onEach { (index, value) ->
                     when (index) {
+                        0 -> {
+                            assertNull(value)
+                            viewModel.requestState()
+                        }
+                        1 -> {
+                            assertNotNull(value)
+                            checkNotNull(value)
+                            assertEquals(value.text, "")
+                        }
                         else -> error("Unexpected index: $index!")
                     }
                 }
-                .take(4)
+                .take(2)
                 .collect()
         }
     }
---
+++ app/src/test/kotlin/useless/android/app/provider/MockLocalDataProvider.kt
@@ -0,0 +1,3 @@
+package useless.android.app.provider
+
+internal class MockLocalDataProvider(override var text: String = "") : LocalDataProvider
