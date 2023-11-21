1b328732c39883da5490dddedfea29c0b8483a06
Stanley Wintergreen <kepocnhh@gmail.com>
2023-11-21 22:02:13 +0300

    update text test

--- app/src/test/kotlin/useless/android/app/module/foo/FooViewModelTest.kt
+++ app/src/test/kotlin/useless/android/app/module/foo/FooViewModelTest.kt
@@ -10,6 +10,7 @@ import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertNull
 import org.junit.Test
 import useless.android.app.module.app.mockInjection
+import useless.android.app.provider.MockLocalDataProvider
 import useless.android.app.provider.mockContexts
 import kotlin.time.Duration.Companion.seconds
 
@@ -39,6 +40,50 @@ internal class FooViewModelTest {
                     }
                 }
                 .take(2)
+                .collect()
+        }
+    }
+
+    @Test
+    fun updateTextTest() {
+        runTest(timeout = 10.seconds) {
+            val initialText = "foobar"
+            val secondText = "barbaz"
+            val injection = mockInjection(
+                contexts = mockContexts(main = coroutineContext),
+                local = MockLocalDataProvider(text = initialText)
+            )
+            val viewModel = FooViewModel(injection)
+            viewModel
+                .state
+                .withIndex()
+                .onEach { (index, value) ->
+                    when (index) {
+                        0 -> {
+                            assertNull(value)
+                            viewModel.requestState()
+                        }
+                        1 -> {
+                            assertNotNull(value)
+                            checkNotNull(value)
+                            assertEquals(value.text, initialText)
+                            viewModel.updateText(secondText)
+                        }
+                        2 -> {
+                            assertNotNull(value)
+                            checkNotNull(value)
+                            assertEquals(value.text, secondText)
+                            viewModel.updateText("")
+                        }
+                        3 -> {
+                            assertNotNull(value)
+                            checkNotNull(value)
+                            assertEquals(value.text, "")
+                        }
+                        else -> error("Unexpected index: $index!")
+                    }
+                }
+                .take(4)
                 .collect()
         }
     }
