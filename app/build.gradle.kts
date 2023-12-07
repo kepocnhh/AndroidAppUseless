337b2e3f8d46ef2b1a5414f56bd4492d502a2107
Stanley Wintergreen <kepocnhh@gmail.com>
2023-12-07 13:07:57 +0300

    fix build script

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -377,15 +377,14 @@ androidComponents.onVariants { variant ->
 }
 
 dependencies {
-    implementation("androidx.activity:activity-compose:1.6.1")
+    implementation("androidx.activity:activity-compose:1.8.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.compose.foundation:foundation:${Version.Android.compose}")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
     debugImplementation("androidx.compose.ui:ui-tooling:${Version.Android.compose}")
     debugImplementation("androidx.compose.ui:ui-tooling-preview:${Version.Android.compose}")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
-    testImplementation("org.robolectric:robolectric:4.11")
     testImplementation("androidx.compose.ui:ui-test-junit4:${Version.Android.compose}")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+    testImplementation("org.robolectric:robolectric:4.11.1")
 
     ktlint("com.pinterest:ktlint:${Version.ktlint}") {
         attributes {
